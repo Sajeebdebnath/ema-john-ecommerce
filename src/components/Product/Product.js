0c7a9daf4e6ca44d9142b8cd705c9745df0c790b
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-01-10 02:25:52 +0600

    Font Awesome install & setup in this project.

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import '../Product/Product.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import "../Product/Product.css";
 
 const Product = (props) => {
-    const { img,  name, price, seller, stock} = props.product;
-    return (
-        <div className='product-item'>
-            <div className='product-image'>
-                <img src={img} alt={name} />
-            </div>
-            <div className='product-content'>
-                <h4 className='product-name'>{name}</h4>
-                <p>by <span>{seller}</span></p>
-                <h5>$ {price}</h5>
-                <p>only <span>{stock}</span> left in stock - order soon</p>
-                <button onClick={()=>props.addCartHandle(props.product)} className='cart_button'>Add to Cart</button>
-            </div>
-        </div>
-    );
+  const { img, name, price, seller, stock } = props.product;
+  return (
+    <div className="product-item">
+      <div className="product-image">
+        <img src={img} alt={name} />
+      </div>
+      <div className="product-content">
+        <h4 className="product-name">{name}</h4>
+        <p>
+          by <span>{seller}</span>
+        </p>
+        <h5>$ {price}</h5>
+        <p>
+          only <span>{stock}</span> left in stock - order soon
+        </p>
+        <button
+          onClick={() => props.addCartHandle(props.product)}
+          className="cart_button">
+          <FontAwesomeIcon icon={faShoppingCart} />
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
