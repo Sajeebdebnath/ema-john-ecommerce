c8fb563d5ed95cb80ce5089fdba5ea272cad4037
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-01-09 18:59:43 +0600

    Navbar Menu added in Website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,6 +6,14 @@ const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="logo" />
+
+            <nav>
+                <ul>
+                    <li><a href="/shop">Shop</a></li>
+                    <li><a href="/order">Order Review</a></li>
+                    <li><a href="/manage">Manage Inventory</a></li>
+                </ul>
+            </nav>
         </div>
     );
 };
