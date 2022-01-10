0660bb64c8fef50c029f54726a8418bddf056f0e
Sajeeb Debnath <sajeebdebnath336@gmail.com>
2022-01-10 13:35:11 +0600

    Cart Component Added in Website

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import '../Cart/Cart.css'
 
 const Cart = (props) => {
+    const cart = props.cart;
+
+    const formatRound = (num) => {
+        const rounded = num.toFixed(2)
+        num = Number(rounded)
+        return num;
+    }
+    
+    let total = 0;
+    for(let i=0; i<cart.length; i++){
+        const product = cart[i];
+        total = total + product.price;
+    }
+
+    let shipping = 0;
+    if(total > 35){
+        shipping =  12.99;
+    }
+    else if(total > 12){
+        shipping = 4.99;
+    }
+    else{
+        shipping = 0;
+    }
+    let total_with_shipping = total+ shipping;
+
+    let tax = total/10;
+    let grand_total = total + shipping + tax
+
+    
+
+
     return (
-        <div>
+        <div className='cart-info'>
             <h2>Order Summery</h2>
-            <p>item ordered : <span>{props.cart.length}</span> </p>
+            <p>item ordered: <span>{cart.length}</span> </p>
+            <table>
+                <tr>
+                    <td>item:</td>
+                    <td>$ {formatRound(total)}</td>
+                </tr>
+                <tr>
+                    <td>Shipping & Handling:</td>
+                    <td>$ {shipping}</td>
+                </tr>
+                <tr>
+                    <td>Total before tax:</td>
+                    <td>$ {formatRound(total_with_shipping)} </td>
+                </tr>
+                <tr>
+                    <td>Estimated Tax:</td>
+                    <td>$ {formatRound(tax)}</td>
+                </tr>
+                <tr className='order-total'>
+                    <td>Order Total:</td>
+                    <td>$ {formatRound(grand_total)}</td>
+                </tr>
+            </table>
+            <button className='cart_button'>Review your order</button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,36 +1,32 @@
-import React,{ useState } from 'react';
-import products from '../../fakeData/products.js';
-import Product from '../Product/Product.js';
-import Cart from '../Cart/Cart.js';
-import '../Shop/Shop.css'
+import React, { useState } from "react";
+import products from "../../fakeData/products.js";
+import Product from "../Product/Product.js";
+import Cart from "../Cart/Cart.js";
+import "../Shop/Shop.css";
 
 const Shop = () => {
-    const first10 = products.slice(0,10);
-    const [product, setProduct] =  useState(first10);
-    const [cart, setCart] = useState([]);
+  const first10 = products.slice(0, 10);
+  const [product, setProduct] = useState(first10);
+  const [cart, setCart] = useState([]);
 
-    const addCartHandle = (product)=>{
-        const newCart = [...cart, product];
-        setCart(newCart)
-    }
+  const addCartHandle = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
 
-    const product_item = product.map((item)=>{
-        return (
-            <Product addCartHandle={addCartHandle} product={item}></Product>
-        )
-    });
-    return (
-        <div className='shop'>
-            <div className='shop-container'>
-                <div className='product-wrapper'>
-                    {product_item}
-                </div>
-                <div className='cart-wrapper'>
-                    <Cart cart={cart}></Cart>
-                </div>
-            </div>
+  const product_item = product.map((item) => {
+    return <Product addCartHandle={addCartHandle} product={item}></Product>;
+  });
+  return (
+    <div className="shop">
+      <div className="shop-container">
+        <div className="product-wrapper">{product_item}</div>
+        <div className="cart-wrapper">
+          <Cart cart={cart}></Cart>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
